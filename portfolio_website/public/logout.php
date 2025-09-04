a9c6d3ae7066c90b138d89cc8e412abdd2ec7797
Sumaiya Akter <sumaiyaakter0248@gmail.com>
2025-09-04 23:36:37 +0600

    Delete portfolio_website/public directory

--- portfolio_website/public/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// public/logout.php
-// Destroys the session (and last_login_at cookie) then redirects to /portfolio/#home
-
-require_once __DIR__ . '/../app/session.php';
-require_once __DIR__ . '/../app/config.php';
-require_once __DIR__ . '/../app/auth.php';
-
-logout_user('#home'); // sends you to /portfolio/#home
