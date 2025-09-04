214e49b51dcdfbb97b01fdbe7a6578a4644b5bec
Sumaiya Akter <sumaiyaakter0248@gmail.com>
2025-09-04 22:34:02 +0600

    Delete portfolio_website/public/hash.php

--- portfolio_website/public/hash.php
+++
@@ -1,5 +0,0 @@
-<?php
-$pw = $_GET['pw'] ?? '';
-header('Content-Type: text/plain; charset=utf-8');
-if ($pw === '') { echo "Usage: hash.php?pw=abcd12345"; exit; }
-echo password_hash($pw, PASSWORD_DEFAULT);
